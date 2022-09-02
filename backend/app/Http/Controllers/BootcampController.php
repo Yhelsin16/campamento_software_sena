9927e9b5bed3085f5ce5595f23b4f1a335b2b703
unknown <jppinzon26@misena.edu.co>
2022-09-02 11:52:21 -0500

    request json

--- backend/app/Http/Controllers/BootcampController.php
+++ backend/app/Http/Controllers/BootcampController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Bootcamp;
 
 class BootcampController extends Controller
 {
@@ -13,7 +14,7 @@ class BootcampController extends Controller
      */
     public function index()
     {
-        return "Aqui se mostraran todos los bootcamps";
+        return Bootcamp::all();
     }
 
     /**
@@ -24,7 +25,11 @@ class BootcampController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Capturo el payload
+        //crear el nuevo bootcamp
+        return Bootcamp::create(
+             $request->all()
+        );
     }
 
     /**
@@ -35,8 +40,7 @@ class BootcampController extends Controller
      */
     public function show($id)
     {
-        return "Mostrar un bootcamp especifico por id";
-
+        return Bootcamp::find($id);
     }
 
     /**
@@ -48,8 +52,12 @@ class BootcampController extends Controller
      */
     public function update(Request $request, $id)
     {
-        return "Actualizar un bootcamp cuyo id sea $id";
-
+        //1. Encontrar el bootcamp por id
+        $b = Bootcamp::find($id);
+        //2. Actualizarlo
+        $b->update($request->all());
+        //3. Enviar response con el bootcamp actulizado
+        return $b;
     }
 
     /**
@@ -60,7 +68,11 @@ class BootcampController extends Controller
      */
     public function destroy($id)
     {
-        return "Eliminar un bootcamp cuyo id sea $id";
-
+        //1. Encontrar el bootcamp eliminar por id
+        $b = Bootcamp::find($id);
+        //2. Eliminarlo
+        $b->delete();
+        //3. Response con el objeto eliminado
+        return $b;
     }
 }
